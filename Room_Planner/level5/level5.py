3bce476fdfc0b58796e6c8df0b4a21a4fbe945f5
Ildikó Cseri <cseriildiko@icloud.com>
2024-11-10 08:55:49 +0100

    refactor

--- Room_Planner/level5/level5.py
+++ Room_Planner/level5/level5.py
@@ -2,33 +2,52 @@
 # SOLUTION #
 ############
 
+def is_horizontal(width, length):
+
+	# perfect room
+	if width % 3 == 2 and length % 2 == 1:
+		return True
+	if width % 2 == 1 and length % 3 == 2:
+		return False
+	
+	# side adjustable
+	if (width - 2) % 3 == 2 and length % 2 == 1:
+		return True
+	if (width - 3) % 2 == 1 and length % 3 == 2:
+		return False
+	
+	# bottom adjustable
+	if (length - 2) % 3 == 2 and width % 2 == 1:
+		return False
+	if (length - 3) % 2 == 1 and width % 3 == 2:
+		return True
+	
+	# both need adjustment
+	if (width - 2) % 3 == 2 or (length - 3) % 2 == 1:
+		return True
+	if (length - 2) % 3 == 2 or (width - 3) % 2 == 1:
+		return False
+	
+	return True
+
 def	room_planner(width, length):
 
 	room = [['.' for _ in range(width)] for _ in range(length)]
 
-	if width % 3 == 2 and width % 2 == 1:
-		if length % 3 == 2:
-			horizontal = False
-		elif length % 2 == 1:
-			horizontal = True
-	if width % 3 == 2 or length % 2 == 1:
-		horizontal = True
-	elif width % 2 == 1 or length % 3 == 2:
-		horizontal = False
-	else:
-		horizontal = False
+	horizontal = is_horizontal(width, length)
 
+	# filling the room with the max possible tables in one direction
 	if horizontal:
 		max_x = width - (4 - (width % 3) * 2)
 		max_y = length - (3 - (length % 2) * 3)
+
+		part_x = [i for i in range(max_x) if i % 3 != 2]
+		part_y = range(0, max_y, 2)
+
 	else:
 		max_x = width - (3 - (width % 2) * 3)
 		max_y = length - (4 - (length % 3) * 2)
 
-	if horizontal:
-		part_x = [i for i in range(max_x) if i % 3 != 2]
-		part_y = range(0, max_y, 2)
-	else:
 		part_x = range(0, max_x, 2)
 		part_y = [i for i in range(max_y) if i % 3 != 2]
 	
@@ -36,7 +55,7 @@ def	room_planner(width, length):
 		for column in part_x:
 			room[row][column] = "X"
 
-
+	# adding tables on the right side if there is space
 	if horizontal:
 		adj_max_x = max_x - (width % 2 == 0)
 		adj_max_y = max_y - (length % 3 == 1) * 2
@@ -58,6 +77,7 @@ def	room_planner(width, length):
 				room[row][column] = "X"
 
 
+	# adding tables on the bottom if there is space
 	if max_y != length:
 		if horizontal:
 			part_x = range(0, adj_max_x, 2)
@@ -70,6 +90,7 @@ def	room_planner(width, length):
 			for column in part_x:
 				room[row][column] = "X"
 
+	# adding tables to the bottom right corner if there is space
 	""" 
 	# this part is incorrect and the maps are incomplete
 
@@ -86,7 +107,7 @@ def	room_planner(width, length):
 			room[-1][-2] = "X"
 			room[-4][-1] = "X"
 			room[-3][-1] = "X"
- """
+	"""
 	return '\n'.join([''.join(line) for line in room]) + "\n"
 
 #########
